3b10ac90d80ae4a1d9fbb18e4b4f2290f8de176e
Erik Borgström <eb223fe@lnu.student.se>
2024-01-19 16:11:05 +0100

    Game Layout
    - Fixed the shipplacement Layout
    - Added Rotate button, that should switch to next turn when all ships for current player is placed

--- src/modules/game/Game.jsx
+++ src/modules/game/Game.jsx
@@ -28,37 +28,62 @@ function Game({ playerOneName, playerTwoName, onCancel }) {
    * @returns A div of the correct players board.
    */
   const divBoard = () => {
-    if (gameLogic.phase === 'placement') {
-      //Should generate their own board
-      //(Current player)
+    if (gameLogic.gamePhase === 'placement') {
+      //Generate their own board
       return (
-        <GameBoard
-          board={gameLogic.currentTurn.playerBoard.board}
-        />
+        <div>
+          <p className='font-bold'>{gameLogic.gamePhase.charAt(0).toUpperCase() + gameLogic.gamePhase.slice(1)} for player: {gameLogic.currentTurn.name.charAt(0).toUpperCase() + gameLogic.currentTurn.name.slice(1)}</p>
+          <p className='font-bold'>Make sure that only {gameLogic.currentTurn.name.charAt(0).toUpperCase() + gameLogic.currentTurn.name.slice(1)} is looking</p>
+          <GameBoard
+            board={gameLogic.currentTurn.playerBoard.board}
+          />
+        </div>
       )
-    } else if (gameLogic.currentTurn === gameLogic.playerOne) {
+    } else if (gameLogic.currentTurn === gameLogic.playerOne && gameLogic.gamePhase !== 'placement') {
       //If this scenario happens it means it is battle and should generate playerTwos board
       return (
-        <GameBoard
-          board={gameLogic.playerTwo.playerBoard.board}
-        />
+        <div>
+          <p className='font-bold'>Red = Hit ---- Blue = Miss --- Green = Sunk</p>
+          <GameBoard
+            board={gameLogic.playerTwo.playerBoard.board}
+          />
+        </div>
       )
-    } else {
+    } else if (gameLogic.currentTurn === gameLogic.playerTwo && gameLogic.gamePhase !== 'placement') {
       //Battle playerTwo turn so playerOne board should be visible
       return (
-        <GameBoard
-          board={gameLogic.playerOne.playerBoard.board}
-        />
+        <div>
+          <p className='font-bold'>Red = Hit ---- Blue = Miss --- Green = Sunk</p>
+          <GameBoard
+            board={gameLogic.playerOne.playerBoard.board}
+          />
+        </div>
       )
     }
   }
 
   return (
     <div>
-      {divBoard()}
-      <ShipPlacement
-        ships={gameLogic.currentTurn.playerShips}
-      />
+      <div className='flex justify-center items-center'>
+        {divBoard()}
+        {gameLogic.gamePhase === 'placement' ? (
+          <div className='item-centered flex-col items-center ml-2'>
+            {gameLogic.currentTurn.playerShips.every(ship => ship.placed) ? (
+              <button className='text-center bg-green-800 bg-opacity-70 p-2 rounded-lg mt-4' type='button' onClick={{/* changeturn */ }}>
+                Next Turn
+              </button>
+            ) : (
+              <button className='text-center bg-green-800 bg-opacity-70 p-2 rounded-lg mt-4' type='button' onClick={{/* Rotate ship */ }}>
+                Rotate
+              </button>
+            )}
+            <ShipPlacement
+              ships={gameLogic.currentTurn.playerShips}
+            />
+          </div>
+        ) : null}
+
+      </div>
       <button className='text-center bg-green-800 bg-opacity-70 p-2 rounded-lg mt-4' type='button' onClick={onCancel}>
         Abort game
       </button>
