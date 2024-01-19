ee2155dc6cbd24eb9741484b3bd6fb973d44a39f
Erik Borgström <eb223fe@lnu.student.se>
2024-01-19 11:04:43 +0100

    Initial setup of project
    - Linting
    - Vitest
    - Vite + React
    - TailwindCss
    
    Added coverage to .gitignore
    Created workflows for git pipeline to run testing and linting on pushes
    Made settings in linting file.

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'indent': ['warn', 2],
+    'quotes': ['warn', 'single'],
+    'semi': ['error', 'never'],
+    'eqeqeq': ['warn', 'always'],
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'warn',
+    'react/no-unused-state': 'warn',
+    'max-lines': ['warn', { max: 300 }],
+    'no-nested-ternary': 'error',
+  },
+}
